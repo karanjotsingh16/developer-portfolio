a85eb5d1b0e91cb301eb84c5834f806515f0680d
Karanjot Singh <107024206+karanjotsingh16@users.noreply.github.com>
2024-02-28 10:25:56 +0530

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,42 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Android App Developer', 'Graphic Designer'],
+    typeSpeed: 50,
+});
+var typed = new Typed('#skills', {
+    strings: ['HTML', 'CSS', 'JavaScript','NodeJs','ReactJs','Java',],
+    typeSpeed: 50,
+});
+
+function mehtiana(){
+    window.location.href="http://www.mountcarmelmehtiana.org/";
+}
+function kakkon(){
+    window.location.href="http://www.mountcarmelhsp.org/";
+}
+function techburner(){
+    window.location.href="https://www.youtube.com/channel/UCXUJJNoP1QupwsYIWFXmsZg";
+}
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
+
